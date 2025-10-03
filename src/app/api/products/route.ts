e1ca2e0c46b0de6c9b91b0db7c09dc1115af7925
Isaaja <111202214416@mhs.dinus.ac.id>
2025-10-03 14:31:09 +0700

    add search by params for product name

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,21 +1,54 @@
 import { prisma } from "@/lib/prismaClient";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import ProductValidator from "@/validator/products";
 import { addProduct } from "@/service/supabase/ProductsService";
 import InvariantError from "@/exceptions/InvariantError";
 import { checkAuth } from "@/app/utils/auth";
+import { Prisma } from "@prisma/client";
 
-export async function GET() {
-  const products = await prisma.product.findMany({
-    include: {
-      category: true,
-    },
-  });
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const productName = searchParams.get("product_name");
+    const sort = searchParams.get("sort") || "createdAt";
+    const order = searchParams.get("order") || "desc";
 
-  return NextResponse.json({
-    status: "success",
-    data: products,
-  });
+    const where = productName
+      ? {
+          product_name: {
+            contains: productName,
+            mode: Prisma.QueryMode.insensitive,
+          },
+        }
+      : {};
+
+    const products = await prisma.product.findMany({
+      where,
+      include: {
+        category: true,
+      },
+      orderBy: {
+        [sort]: order === "asc" ? "asc" : "desc",
+      },
+    });
+
+    return NextResponse.json(
+      {
+        status: "success",
+        data: products,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return NextResponse.json(
+      {
+        status: "error",
+        message: "Internal Server Error",
+      },
+      { status: 500 }
+    );
+  }
 }
 
 export async function POST(req: Request) {
