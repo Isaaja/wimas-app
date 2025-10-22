e7513e280275e3433f9ce29b9105f3b50e7fa672
FariRiadi17 <riadifari@gmail.com>
2025-10-22 22:23:40 +0700

    update endpoint LLM

--- python-script/venv/Scripts/activate.bat
+++ python-script/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Kerja Praktek\wisma\python-script\venv
+set VIRTUAL_ENV=D:\UDINUS\Kerja Praktek\wisma-app\python-script\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
