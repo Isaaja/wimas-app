c06b5a7118f26e3937c0489a669e332f00183654
Isaaja <isaiantmaulana2004@gmail.com>
2026-01-19 16:22:00 +0700

    eslint disabled

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   env: {
     ACCESS_TOKEN_KEY: process.env.ACCESS_TOKEN_KEY,
     REFRESH_TOKEN_KEY: process.env.REFRESH_TOKEN_KEY,
